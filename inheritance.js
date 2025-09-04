fa1ceb4b849d1abd688352a20463b2db07b2f373
Md. Rony <ronimdrahim467@gmail.com>
2025-09-04 11:18:16 +0600

    kono mote hoise aber rivision deya lagbe

---
+++ inheritance.js
@@ -0,0 +1,23 @@
+
+class Dog {
+    constructor(name,bark,behabihor){
+        this.name= name;
+        this.bark=bark;
+        this.kale=behabihor;
+    }
+    eat(){
+        console.log(`${this.name} is etting`);
+    }
+    bark(){
+        console.log(`${this.bark} is barking`);
+    }
+    kala(){
+        console.log(`${this.behabihor} valo na`)
+    }
+}
+const dog =new Dog('Kukur' ,'kamray');
+const dog2 =new Dog('ato-sad ', 'kukur mere');
+const manar=new Dog('manar nai' ,' saradin voke');
+console.log(dog);
+console.log(dog2);
+console.log(manar);
